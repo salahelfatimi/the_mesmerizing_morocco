0b11fee54dc714fd7c43542b1b8b639cfab35823
salahelfatimi <salahfatimi16@gmail.com>
2025-03-20 00:37:18 +0000

    Update Home component to use async function and adjust layout for full height

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -32,19 +32,19 @@ export async function generateMetadata() {
   }
 };
 
-export default  function Home() {
+export default async function Home() {
 
   return (
-    <div className="flex lg:flex-row  ">
-      <div className="flex items-center justify-center bg-primary w-14 h-full ">
-        <h2 className="transform text-white uppercase rotate-90 text-2xl font-bold whitespace-nowrap">
+    <div className="flex lg:flex-row  h-screen">
+      <div className="flex items-center justify-center bg-primary w-14 ">
+        <h2 className="transform text-white uppercase rotate-90 text-2xl  font-bold whitespace-nowrap">
           We are launching soon
         </h2>
       </div>
       <div className="lg:w-1/3 hidden lg:block ">
         <Image_gsap/>
       </div>
-      <div className=" lg:w-2/3 h-full flex flex-col container justify-center items-center w-full">
+      <div className=" lg:w-2/3 flex flex-col container justify-center items-center w-full">
         <div>
           <div className="flex flex-col items-center">
             <Image src={'/img/logo_text.jpg'} width={500} height={500} className="w-28" alt="the mesmerizing morocco" title="the mesmerizing morocco" />
